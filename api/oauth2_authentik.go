15bc48ada17136d9442b87849cf0dd70bede6518
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-14 03:15:52 +0200

    Test 6

--- api/oauth2_authentik.go
+++ api/oauth2_authentik.go
@@ -86,7 +86,7 @@ func (conf *oauth2Config) getAuthentikUserInfo(ctx context.Context, token *oauth
 	err = json.Unmarshal(content, &u)
 	// err = json.NewDecoder(response.Body).Decode(&u)
 	if err != nil {
-		return userInfo{}, fmt.Errorf("getAuthentikUserInfo - Unmarshal: %s\n%w", content, err)
+		return userInfo{}, fmt.Errorf("getAuthentikUserInfo - Unmarshal failed: %s\nJSON content: %s", err.Error(), string(content))
 	}
 
 	return userInfo{
