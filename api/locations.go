1ba296572fa2f814ff79c2b326b0ed6c5f87be78
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-21 15:34:45 +0100

    increased limit from 20 to 200

--- api/locations.go
+++ api/locations.go
@@ -10,7 +10,7 @@ import (
 )
 
 type getLocationsRequest struct {
-	Limit  int32  `form:"limit,default=20"`
+	Limit  int32  `form:"limit,default=200"`
 	Offset int32  `form:"offset"`
 	Type   string `form:"type"`
 }
