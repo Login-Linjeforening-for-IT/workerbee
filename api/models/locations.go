56529b5535ff56fda1ef4867f648ba9a1dad3865
0herm <0herm@protonmail.com>
2025-10-17 19:28:12 +0200

    fix location validation

--- api/models/locations.go
+++ api/models/locations.go
@@ -11,8 +11,8 @@ type LocationBase struct {
 	MazemapPoiID    *int                `db:"mazemap_poi_id" json:"mazemap_poi_id,omitempty"`
 	AddressStreet   *string             `db:"address_street" json:"address_street,omitempty"`
 	AddressPostcode *int                `db:"address_postcode" json:"address_postcode,omitempty"`
-	CoordinateLat   *float64            `db:"coordinate_lat" json:"coordinate_lat,omitempty"`
-	CoordinateLon   *float64            `db:"coordinate_lon" json:"coordinate_lon,omitempty"`
+	CoordinateLat   *float64            `db:"coordinate_lat" json:"coordinate_lat,omitempty" validate:"omitempty,gte=-90,lte=90"`
+	CoordinateLon   *float64            `db:"coordinate_lon" json:"coordinate_lon,omitempty" validate:"omitempty,gte=-180,lte=180"`
 	URL             *string             `db:"url" json:"url,omitempty" validate:"omitempty"`
 	CreatedAt       *internal.LocalTime `db:"created_at" json:"created_at"`
 	UpdatedAt       *internal.LocalTime `db:"updated_at" json:"updated_at"`
