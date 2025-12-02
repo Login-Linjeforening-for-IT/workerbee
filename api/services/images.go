a942e4e6d6c9f571c88924b39c3c2726f6fe0355
OleMarGlo <om.glomsrud@gmail.com>
2025-12-02 18:58:34 +0100

    fix image compression

--- api/services/images.go
+++ api/services/images.go
@@ -61,54 +61,20 @@ func (is *ImageService) UploadImage(file *multipart.FileHeader, ctx context.Cont
 		return "", err
 	}
 
-	width, height := img.Bounds().Dx(), img.Bounds().Dy()
-
-	newWidth, newHeight := internal.DownscaleImage(width, height)
-
-	if newWidth != width || newHeight != height {
-		img = imaging.Resize(img, newWidth, newHeight, imaging.Lanczos)
+	w, h := img.Bounds().Dx(), img.Bounds().Dy()
+	newW, newH := internal.DownscaleImage(w, h)
+	if newW != w || newH != h {
+		img = imaging.Resize(img, newW, newH, imaging.Lanczos)
 	}
 
-	quality := 85
-	var buf bytes.Buffer
-
-	for quality >= 50 {
-		buf.Reset()
-		err = webp.Encode(&buf, img, &webp.Options{Lossless: false, Quality: float32(quality)})
-		if err != nil {
-			return "", err
-		}
-
-		if buf.Len() <= int(internal.MaxImageSize) {
-			break
-		}
-		quality -= 10
-	}
-
-	for buf.Len() > int(internal.MaxImageSize) {
-		bounds := img.Bounds()
-		width := bounds.Dx()
-		height := bounds.Dy()
-
-		newWidth := int(float64(width) * .9)
-		newHeight := int(float64(height) * .9)
-
-		if newWidth < 100 || newHeight < 100 {
-			break
-		}
-
-		img = imaging.Resize(img, newWidth, newHeight, imaging.Lanczos)
-
-		buf.Reset()
-		err = webp.Encode(&buf, img, &webp.Options{Lossless: false, Quality: 75})
-		if err != nil {
-			return "", err
-		}
+	buf := new(bytes.Buffer)
+	if err := webp.Encode(buf, img, &webp.Options{Lossless: false, Quality: 80}); err != nil {
+		return "", err
 	}
 
 	key := internal.IMG_PATH + path + strings.Split(file.Filename, ".")[0] + ".webp"
 
-	err = is.repo.UploadImage(ctx, key, "image/webp", &buf)
+	err = is.repo.UploadImage(ctx, key, "image/webp", buf)
 	if err != nil {
 		return "", err
 	}
