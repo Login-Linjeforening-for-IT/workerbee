497e6aa098c60bac7a376e981632ab07745c56ed
OleMarGlo <om.glomsrud@gmail.com>
2026-02-10 21:16:17 +0100

    put

--- api/routes_internal/router.go
+++ api/routes_internal/router.go
@@ -306,7 +306,7 @@ func Route(c *gin.Engine, h *handlers.Handler) {
 				middleware.RateLimitMiddleware(config.AllowedRequestsPerMinute),
 				h.SetAlbumCover,
 			)
-			albums.POST(
+			albums.PUT(
 				"/compress",
 				middleware.AuthMiddleware(),
 				middleware.RateLimitMiddleware(1),
